bcf9dd6bedf0a8741c6071d24eb615e0d2b35e64
zhoang-2 <huyhoangzzzzb@gmail.com>
2024-03-14 23:57:35 +0700

    records

--- src/utils/songs.js
+++ src/utils/songs.js
@@ -1,64 +1,65 @@
 import cover from "../accests/images/3107.jpg"
 import cover2 from "../accests/images/ChungTaCuaSauNay.jpg"
-import { FaPlay } from "react-icons/fa";
+
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
 export const songs = [
     {
         title:"3107",
         singer:"ft. Nâu, Duongg ",
         link:"/3107",
         cover:cover,
-        icon:FaPlay,
+        icon:faPlay,
     },
     {
         title:"Chúng Ta Sau Này",
         singer:"Bài hát của T.R.I ",
         link:"/chung-ta-sau-nay",
         cover:cover2,
-        icon:FaPlay,
+        icon:faPlay,
     },{
         title:"3107",
         singer:"ft. Nâu, Duongg ",
         link:"/3107",
         cover:cover,
-        icon:FaPlay,
+        icon:faPlay,
     },
     {
         title:"Chúng Ta Sau Này",
         singer:"Bài hát của T.R.I ",
         link:"/chung-ta-sau-nay",
         cover:cover2,
-        icon:FaPlay,
+        icon:faPlay,
     },{
         title:"3107",
         singer:"ft. Nâu, Duongg ",
         link:"/3107",
         cover:cover,
-        icon:FaPlay,
+        icon:faPlay,
     },
     {
         title:"Chúng Ta Sau Này",
         singer:"Bài hát của T.R.I ",
         link:"/chung-ta-sau-nay",
         cover:cover2,
-        icon:FaPlay,
+        icon:faPlay,
     },{
         title:"3107",
         singer:"ft. Nâu, Duongg ",
         link:"/3107",
         cover:cover,
-        icon:FaPlay,
+        icon:faPlay,
     },
     {
         title:"Chúng Ta Sau Này",
         singer:"Bài hát của T.R.I ",
         link:"/chung-ta-sau-nay",
         cover:cover2,
-        icon:FaPlay,
+        icon:faPlay,
     },{
         title:"3107",
         singer:"ft. Nâu, Duongg ",
         link:"/3107",
         cover:cover,
-        icon:FaPlay,
+        icon:faPlay,
     }
 ];
