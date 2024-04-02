cc985bf92872bb3c2cb182c0ab7112eeac4596f1
Bùi Tấn Phát <103013376+krphat@users.noreply.github.com>
2024-04-02 21:57:59 +0700

    handled when selecting music in an album

--- src/screens/Albums/PlayListAlbum.jsx
+++ src/screens/Albums/PlayListAlbum.jsx
@@ -3,25 +3,39 @@ import { useNavigate } from "react-router-dom";
 
 const PlayListAlbum = (props) => {
   const history = useNavigate();
-  console.log("PLAY LIST ALBUM", props.item);
+  // console.log("PLAY LIST ALBUM", props.item);
+  const pos = props.item.snippet.position;
+  console.log("INDEX", pos);
 
   const handleClickSong = () => {
     history(
       `/watch/album/${props.item.snippet.resourceId
         .videoId}?title=${props.item.snippet.title}&channel=${props.item.snippet.channelId}`,
-        {state: {songs: props.songs}}
+        {state: {songs: props.songs, idx: pos}}
     );
   };
   return (
-    <div className="video-recommend" onClick={handleClickSong}>
-      <div
-        className="thumb"
-        style={{
-          backgroundImage: `url(${props.item.snippet.thumbnails.high.url})`,
-        }}
-      ></div>
-      <div className="title">{props.item.snippet.title}</div>
-    </div>
+    props.curPlay === pos ? (
+      <div className="video-recommend" onClick={handleClickSong} style={{backgroundColor: "rgba(89, 107, 99, 0.5)"}}>
+        <div
+          className="thumb"
+          style={{
+            backgroundImage: `url(${props.item.snippet.thumbnails.high.url})`,
+          }}
+        ></div>
+        <div className="title" style={{fontWeight: "bold"}}>{props.item.snippet.title}</div>
+      </div>
+    ) : (
+      <div className="video-recommend" onClick={handleClickSong}>
+        <div
+          className="thumb"
+          style={{
+            backgroundImage: `url(${props.item.snippet.thumbnails.high.url})`,
+          }}
+        ></div>
+        <div className="title">{props.item.snippet.title}</div>
+      </div>
+    )
   );
 };
 
