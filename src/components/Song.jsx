15e02c61e5f3fde388f05c08c283340b5ed2a6b8
Bùi Tấn Phát <103013376+krphat@users.noreply.github.com>
2024-05-05 22:54:45 +0700

    fix play song screen

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -4,21 +4,22 @@ import { useNavigate } from "react-router-dom";
 
 const Song = (props) => {
   const history = useNavigate();
-  const channelId = props.item.channelId;
+  const channelId = props.item.snippet.channelId;
 
   const handleClickReSong = () => {
     history(
-      `/watch/${props.item.videoId}?title=${props.item.title}&channel=${channelId}`
+      `/watch/${props.item.snippet.resourceId.videoId}?title=${props.item.snippet.title}&channel=${props.item.snippet.videoOwnerChannelId
+      }`
     );
   };
   const handleClickSong = () => {
     history(
-      `/watch/${props.item.id}?title=${props.item.title}&channel=${channelId}`
+      `/watch/${props.item.id}?title=${props.item.snippet.title}&channel=${channelId}`
     );
   };
   const handleClickSongInSearchScreen = () => {
     history(
-      `/watch/${props.item.videoId}?title=${props.item.title}&channel=${channelId}`
+      `/watch/${props.item.id.videoId}?title=${props.item.snippet.title}&channel=${channelId}`
     );
   };
 
@@ -27,7 +28,7 @@ const Song = (props) => {
   let functionChosed;
   if (props.item.id.videoId) {
     functionChosed = handleClickSongInSearchScreen;
-  } else if (props.item.videoId) {
+  } else if (props.item.snippet.resourceId.videoId) {
     functionChosed = handleClickReSong;
   } else {
     functionChosed = handleClickSong;
@@ -46,19 +47,19 @@ const Song = (props) => {
       <div
         className="thumb-song"
         style={{
-          backgroundImage: `url(${props.item.music.thumbnails})`,
+          backgroundImage: `url(${props.item.snippet.thumbnails.high.url})`,
         }}
       ></div>
       <div className="detail-song">
-        <span className="name-song">{props.item.music.title}</span>
+        <span className="name-song">{props.item.snippet.title}</span>
         <div className="information-song">
           <div className="chanel-singer">
             <span style={{ color: "gray", fontSize: "14px" }}>
-              {props.item.music.channelTitle}
+              {props.item.snippet.channelTitle}
             </span>
           </div>
           <span className="custom-decripton">
-            Mô tả: {props.item.music.description}
+            Mô tả: {props.item.snippet.description}
           </span>
         </div>
       </div>
--- src/components/VideoRecommend.jsx
+++ src/components/VideoRecommend.jsx
@@ -3,10 +3,10 @@ import { useNavigate } from "react-router-dom";
 
 const VideoRecommend = (props) => {
   const history = useNavigate();
-
+  console.log("Prop song recom: ", props.item);
   const handleClickSong = () => {
     history(
-      `/watch/${props.item.videoId}?title=${props.item.title}&channel=${props.item.channelId}`
+      `/watch/${props.item.id.videoId}?title=${props.item.snippet.title}&channel=${props.item.snippet.channelId}`
     );
   };
   return (
@@ -14,10 +14,10 @@ const VideoRecommend = (props) => {
       <div
         className="thumb"
         style={{
-          backgroundImage: `url(${props.item.thumbnails})`,
+          backgroundImage: `url(${props.item.snippet.thumbnails.high.url})`,
         }}
       ></div>
-      <div className="title">{props.item.title}</div>
+      <div className="title">{props.item.snippet.title}</div>
     </div>
   );
 };
